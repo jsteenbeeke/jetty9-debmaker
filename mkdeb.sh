576a6f695d98703719605286cf98e5a9abaa09fb
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2015-09-17 14:44:28 +0200

    Functional deb created

--- mkdeb.sh
+++ mkdeb.sh
@@ -9,11 +9,6 @@ USR_LIB_JETTY="lib modules VERSION.txt README.TXT license-eplv10-aslv20.html sta
 ETC_JETTY="etc"
 BASE=`pwd`
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 if [ $# == 1 ]; then
 	if [ -d $1 ]; then
 		if [ -d target ]; then
@@ -22,40 +17,45 @@ if [ $# == 1 ]; then
 		fi
 
 		# Create data folder
-		mkdir -v -p $TARGET_DATA
+		fakeroot mkdir -p $TARGET_DATA
 
-		mkdir -v -p $TARGET_DATA/etc/init.d/
-		mkdir -v -p $TARGET_DATA/etc/jetty9/
-		mkdir -v -p $TARGET_DATA/usr/share/jetty9/
-		mkdir -v -p $TARGET_DATA/var/log/jetty9/
-		mkdir -v -p $TARGET_DATA/var/lib/jetty9/
+		echo Creating folders
+
+		fakeroot mkdir -p $TARGET_DATA/etc/init.d/
+		fakeroot mkdir -p $TARGET_DATA/etc/default/
+		fakeroot mkdir -p $TARGET_DATA/etc/jetty9/
+		fakeroot mkdir -p $TARGET_DATA/usr/share/jetty9/
+		fakeroot mkdir -p $TARGET_DATA/var/log/jetty9/
+		fakeroot mkdir -p $TARGET_DATA/var/cache/jetty9/
+		fakeroot mkdir  -p $TARGET_DATA/var/lib/jetty9/
 
 		cd $1
 
-		echo /etc/jetty9
+		echo Copying config files
 		for i in $ETC_JETTY; do
-				cp -r $i/* $BASE/$TARGET_DATA/etc/jetty9/
+			fakeroot cp -r $i/* $BASE/$TARGET_DATA/etc/jetty9/
 		done
+		
+		fakeroot cp $BASE/etc-default-jetty $BASE/$TARGET_DATA/etc/default/jetty9
 
-		echo /usr/share/jetty9
+		echo Copying default files
 		for i in $USR_LIB_JETTY; do
-			cp -r $i $BASE/$TARGET_DATA/usr/share/jetty9/
+			fakeroot cp -r $i $BASE/$TARGET_DATA/usr/share/jetty9/
 		done
 
-		echo /
-
-		cp -v bin/jetty.sh $BASE/$TARGET_DATA/etc/init.d/jetty9
-		cp -v $BASE/etc-default-jetty $BASE/$TARGET_DATA/etc/default/jetty9
+		fakeroot cp bin/jetty.sh $BASE/$TARGET_DATA/etc/init.d/jetty9
 
 		cd $BASE/$TARGET_DATA/
 
+		echo Creating symlinks
+
 		cd var/lib/jetty9
-		ln -v -s /usr/share/jetty9/webapps webapps
+		fakeroot ln -s /usr/share/jetty9/webapps webapps
 		cd usr/share/jetty9
-		ln -v -s /etc/jetty9 etc
+		fakeroot ln -s /etc/jetty9 etc
 		cd ../../..
 
-		tar --lzma -cvf ../data.tar.xz *	
+		fakeroot tar --lzma -cf ../data.tar.xz *	
 
 		cd $BASE
 
@@ -64,11 +64,10 @@ if [ $# == 1 ]; then
 		INSTALLED_SIZE=${DU_INSTALLED_SIZE[0]}
 		VERSION=`cat $TARGET_DATA/usr/share/jetty9/VERSION.txt | grep -m1 ^jetty | sed -e 's/^jetty-\([^\s]\+\)/\1/g' | sed -e 's/\s.*$//'`
 
-	
-		mkdir -p $TARGET_CONTROL
-		cp -r $BASE/control/* $TARGET_CONTROL
+		echo Creating control file	
 
-		echo "Generating: control"
+		fakeroot mkdir -p $TARGET_CONTROL
+		fakeroot cp -r $BASE/control/* $TARGET_CONTROL
 
 		echo "Package: jetty9" > $TARGET_CONTROL/control
 		echo "Version: $VERSION" >> $TARGET_CONTROL/control
@@ -92,33 +91,37 @@ if [ $# == 1 ]; then
 		
 		for i in `find *`; do
 			if [ ! -d $i ]; then
-				echo /etc/$i >> $BASE/$TARGET_CONTROL/conffiles
+				fakeroot echo /etc/$i >> $BASE/$TARGET_CONTROL/conffiles
 			fi
 		done
 
-		chmod 644 $BASE/$TARGET_CONTROL/conffiles
-		chmod 755 $BASE/$TARGET_CONTROL/postinst
-		chmod 755 $BASE/$TARGET_CONTROL/prerm
-		chmod 755 $BASE/$TARGET_CONTROL/postrm
+		echo "Setting file permissions"
+
+		fakeroot chmod 644 $BASE/$TARGET_CONTROL/conffiles
+		fakeroot chmod 755 $BASE/$TARGET_CONTROL/postinst
+		fakeroot chmod 755 $BASE/$TARGET_CONTROL/prerm
+		fakeroot chmod 755 $BASE/$TARGET_CONTROL/postrm
 
 		echo "Generating: md5sums"
 
 		cd $BASE/$TARGET_DATA/
 
-		touch $BASE/$TARGET_CONTROL/md5sums
-		chmod 644 $BASE/$TARGET_CONTROL/md5sums
+		fakeroot touch $BASE/$TARGET_CONTROL/md5sums
+		fakeroot chmod 644 $BASE/$TARGET_CONTROL/md5sums
 
-		find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > $BASE/$TARGET_CONTROL/md5sums
+		fakeroot find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > $BASE/$TARGET_CONTROL/md5sums
 
 		echo "Generating: debian-binary"
 
-		echo "2.0" >> $BASE/$TARGET/debian-binary
+		fakeroot echo "2.0" >> $BASE/$TARGET/debian-binary
 
 		cd $BASE/$TARGET/
 
+		echo Fixing directory file permissions
+
 		for i in `find *`; do
 			if [ -d $i ]; then
-				chmod 755 $i
+				fakeroot chmod 755 $i
 			fi
 		done
 
@@ -126,19 +129,19 @@ if [ $# == 1 ]; then
 
 		cd $BASE/$TARGET_DATA/
 
-		tar --lzma -cvf $BASE/$TARGET/data.tar.xz *
+		fakeroot tar --lzma -cf $BASE/$TARGET/data.tar.xz *
 
 		echo "Packaging control"
 
 		cd $BASE/$TARGET_CONTROL/
 
-		tar -czf $BASE/$TARGET/control.tar.gz *
+		fakeroot tar -czf $BASE/$TARGET/control.tar.gz *
 
 		cd $BASE/$TARGET
 
-		ar -r jetty-$VERSION.deb debian-binary control.tar.gz data.tar.xz
+		fakeroot ar -r jetty9-$VERSION.deb debian-binary control.tar.gz data.tar.xz
 
-		lintian jetty-$VERSION.deb --suppress-tags unknown-java-class-version
+		lintian jetty9-$VERSION.deb --suppress-tags unknown-java-class-version
 	else
 		echo $1 is not a folder
 	fi
